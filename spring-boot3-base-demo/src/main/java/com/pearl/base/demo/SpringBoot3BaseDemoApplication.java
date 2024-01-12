acc4849f9f2745d3f1c3d035b41a065a9adbe2e3
chufan <lrnimo@163.com>
2024-01-12 17:44:40 +0800

    init

---
+++ spring-boot3-base-demo/src/main/java/com/pearl/base/demo/SpringBoot3BaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pearl.base.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3BaseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3BaseDemoApplication.class, args);
+    }
+
+}
---
+++ spring-boot3-base-demo/src/test/java/com/pearl/base/demo/SpringBoot3BaseDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pearl.base.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3BaseDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
